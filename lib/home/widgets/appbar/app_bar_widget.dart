6dad22269a34a64b102391694fac75717e098c77
Danivaldo Sousa <danivaldosousa@192.168.1.9>
2021-04-20 18:05:32 -0300

    Add Page Home anybody part, Add Page Splash

---
+++ lib/home/widgets/appbar/app_bar_widget.dart
@@ -0,0 +1,53 @@
+import 'package:DevQuiz/core/core.dart';
+import 'package:DevQuiz/home/widgets/score_card/score_card_widget.dart';
+import 'package:flutter/material.dart';
+
+class AppBarWidget extends PreferredSize {
+  AppBarWidget()
+      : super(
+          preferredSize: Size.fromHeight(250),
+          child: Container(
+            height: 250,
+            child: Stack(
+              children: [
+                Container(
+                  height: 161,
+                  width: double.maxFinite,
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  decoration: BoxDecoration(
+                    gradient: AppGradients.linear,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text.rich(TextSpan(
+                          text: 'Olá, ',
+                          style: AppTextStyles.title,
+                          children: [
+                            TextSpan(
+                                text: 'Danivaldo',
+                                style: AppTextStyles.titleBold),
+                          ])),
+                      Container(
+                        height: 58,
+                        width: 58,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          image: DecorationImage(
+                            image: NetworkImage(
+                                "https://avatars.githubusercontent.com/u/8021142?v=4"),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Align(
+                  alignment: Alignment(0.0, 1.0),
+                  child: ScoreCardWidget(),
+                ),
+              ],
+            ),
+          ),
+        );
+}
