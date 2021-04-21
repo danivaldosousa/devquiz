19bb47dcea4a9bd647c18e08196a1d87bb766f68
Danivaldo Sousa <danivaldosousa@192.168.1.9>
2021-04-21 14:55:30 -0300

    Done the tasks of the Questions and Answers page, without the functionalities and connection with api.

---
+++ lib/challenge/widgets/questio_indicator/question_indicator_widget.dart
@@ -0,0 +1,34 @@
+import 'package:DevQuiz/core/core.dart';
+import 'package:DevQuiz/shared/widgets/progress_indicator/progress_indicator_widget.dart';
+import 'package:flutter/material.dart';
+
+class QuestionIndicatorWidget extends StatelessWidget {
+  const QuestionIndicatorWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: const EdgeInsets.symmetric(horizontal: 24),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Questão 4",
+                  style: AppTextStyles.body,
+                ),
+                Text(
+                  "10",
+                  style: AppTextStyles.body,
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            ProgressIndicatorWidget(value: 0.7),
+          ],
+        ));
+  }
+}
